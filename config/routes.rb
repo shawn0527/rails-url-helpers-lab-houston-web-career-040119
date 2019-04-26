612a6e51c7d8e5616ff3f18fa19cae88e01435a1
Shawn Ren <shawnren527@gmail.com>
2019-04-26 18:27:55 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # patch '/students/:id/active', to: 'students#active'
   resources :students, only: [:edit, :index, :show]
-  get '/students/:id/activate', to: 'students#edit', as: 'activate_student_path'
+  get '/students/:id/activate', to: 'students#edit', as: 'activate'
 end
